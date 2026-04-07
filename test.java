f467ccc8f624a1e76d75a10032d625de6505efe3
苦学Java <435224465@qq.com>
2026-04-07 15:27:36 +0800

    Revert "重置test.java为基础版本"
    This reverts commit 74b2d20c641a4dcc3385e043852bbdb6f72aea12.

--- test.java
+++ test.java
@@ -1,6 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("合并后的最终版本");
-        System.out.println("dev最终版本");
+        System.out.println("修改后的test.java内容");
     }
 }
